b96294a3d7639a67cdc3ac0fbfa98502354eeb20
FarciarzYT <140611697+FarciarzYT@users.noreply.github.com>
2024-11-16 14:31:43 +0100

    Upgraded so code is not printed in calc() but in main()

--- main.py
+++ main.py
@@ -2,15 +2,13 @@ import math
 
 def calc(x):
     if x < 30000:
-        return print("Jest to kwota zwolniona z podatku.")
+        return 0  # Zwolniona z podatku
     elif x <= 120000:
         return math.ceil((x * 0.12) - 3600)
-    elif x > 120000 and x <= 1000000:
+    elif x <= 1000000:
         return math.ceil(10800 + 0.32 * (x - 120000))
-    elif x > 1000000:
-        return math.ceil(10800 + 0.32 * (x - 120000) + 0.04 * (x - 1000000))
     else:
-        return 0  
+        return math.ceil(10800 + 0.32 * (x - 120000) + 0.04 * (x - 1000000))
 
 def main():
     try:
@@ -19,7 +17,10 @@ def main():
             print("Błąd: Wprowadzona kwota nie może być ujemna.")
         else:
             podatek = calc(price)
-            print(f"Obliczony podatek: {podatek} zł")
+            if podatek == 0:
+                print("Jest to kwota zwolniona z podatku.")
+            else:
+                print(f"Obliczony podatek: {podatek} zł")
     except ValueError:
         print("Błąd: Wprowadź prawidłową liczbę.")
 
